fe26ae585009d777f5e3acb176fe208fa35c370c
desarrollador01 <ivanandres26@hotmail.es>
2025-11-29 22:54:08 -0500

    Agregar front al repo principal

---
+++ BackPruebaTecnicaViva1a/PruebaTecnicaViva1a/Models/DTO/PatientResultDto.cs
@@ -0,0 +1,16 @@
+﻿namespace PruebaTecnicaViva1a.Models.DTO
+{
+    public class PatientResultDto
+    {
+        public int PatientId { get; set; }
+        public string DocumentType { get; set; }
+        public string DocumentNumber { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public DateTime? BirthDate { get; set; }
+        public string? Email { get; set; }
+        public string? PhoneNumber { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+
+}
---
+++ BackPruebaTecnicaViva1a/PruebaTecnicaViva1a/Test/DbContextMock.cs
@@ -0,0 +1,18 @@
+﻿namespace PruebaTecnicaViva1a.Test
+{
+    using Microsoft.EntityFrameworkCore;
+    using PruebaTecnicaViva1a.Models.DB;
+
+    public static class DbContextMock
+    {
+        public static PruebaTecnicaContext GetContext(string dbName)
+        {
+            var options = new DbContextOptionsBuilder<PruebaTecnicaContext>()
+                .UseInMemoryDatabase(databaseName: dbName)
+                .Options;
+
+            return new PruebaTecnicaContext(options);
+        }
+    }
+
+}
